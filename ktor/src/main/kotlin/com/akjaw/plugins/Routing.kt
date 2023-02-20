8c2a915dd48829d208548706afe5b41ca8ac6d02
AKJAW <akjaworski1@gmail.com>
2023-02-20 20:45:18 +0100

    feat: add fruit API call to ktor sever

--- ktor/src/main/kotlin/com/akjaw/plugins/Routing.kt
+++ ktor/src/main/kotlin/com/akjaw/plugins/Routing.kt
@@ -1,26 +1,31 @@
 package com.akjaw.plugins
 
-import io.ktor.server.routing.*
-import io.ktor.server.response.*
-import io.ktor.server.resources.*
-import io.ktor.resources.*
+import com.akjaw.fruit.KtorFruitApi
+import io.ktor.resources.Resource
+import io.ktor.server.application.Application
+import io.ktor.server.application.call
+import io.ktor.server.application.install
 import io.ktor.server.resources.Resources
+import io.ktor.server.resources.get
+import io.ktor.server.response.respond
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.get
+import io.ktor.server.routing.routing
 import kotlinx.serialization.Serializable
-import io.ktor.server.application.*
 
 fun Application.configureRouting() {
+    val client = KtorFruitApi()
     install(Resources)
     routing {
         get("/") {
             call.respondText("Hello World!")
         }
-        get<Articles> { article ->
-            // Get all articles ...
-            call.respond("List of articles sorted starting from ${article.sort}")
+        get<Fruits> {
+            call.respond(client.getFruits())
         }
     }
 }
 
 @Serializable
-@Resource("/articles")
-class Articles(val sort: String? = "new")
+@Resource("/fruits")
+class Fruits
