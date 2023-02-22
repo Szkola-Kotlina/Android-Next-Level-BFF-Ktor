4c95b3740974762dface39f95d750c279b6431df
AKJAW <akjaworski1@gmail.com>
2023-02-22 17:09:53 +0100

    feat: add API favoriting

--- app/src/main/java/com/akjaw/android/next/level/ktor/fruit/FruitApi.kt
+++ app/src/main/java/com/akjaw/android/next/level/ktor/fruit/FruitApi.kt
@@ -1,51 +1,106 @@
 package com.akjaw.android.next.level.ktor.fruit
 
 import android.util.Log
+import com.akjaw.android.next.level.ktor.shared.authentication.VALID_USERS
 import com.akjaw.android.next.level.ktor.shared.model.FruitSchema
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.request.delete
 import io.ktor.client.request.get
+import io.ktor.client.request.header
+import io.ktor.client.request.headers
+import io.ktor.client.request.post
 import io.ktor.client.statement.HttpResponse
+import io.ktor.http.HttpStatusCode
 import io.ktor.serialization.kotlinx.json.*
 import kotlinx.serialization.json.Json
 
 interface FruitApi {
 
     suspend fun getFruits(): List<FruitSchema>
+
+    suspend fun getFavorites(): List<Int>
+
+    suspend fun addFavorite(fruitId: Int): Boolean
+
+    suspend fun removeFavorite(fruitId: Int): Boolean
 }
 
-fun createFruitApi(): FruitApi = createInternalFruitApi()
+fun createFruitApi(): FruitApi = createOfficialFruitApi()
 
-fun createInternalFruitApi(): FruitApi {
+private fun createInternalFruitApi(): FruitApi {
     val client: HttpClient = createClient()
     return KtorFruitApi(
-        getFruitsFromApi = { client.get("http://0.0.0.0:8080/fruits") }
+        getFruitsApi = { client.get("http://0.0.0.0:8080/fruits").body() },
+        getFavoritesApi = { client.get("http://0.0.0.0:8080/fruits/favorites").body() },
+        addFavoriteApi = { fruitId ->
+            val response = client.post("http://0.0.0.0:8080/fruits/favorites") {
+                url { parameters.append("id", "$fruitId") }
+            }
+            response.status == HttpStatusCode.OK
+        },
+        removeFavoriteApi = { fruitId ->
+            val response = client.delete("http://0.0.0.0:8080/fruits/favorites") {
+                url { parameters.append("id", "$fruitId") }
+            }
+            response.status == HttpStatusCode.OK
+        }
     )
 }
 
-fun createOfficialFruitApi(): FruitApi {
+private fun createOfficialFruitApi(): FruitApi {
     val client: HttpClient = createClient()
     return KtorFruitApi(
-        getFruitsFromApi = { client.get("https://www.fruityvice.com/api/fruit/all") }
+        getFruitsApi = { client.get("https://www.fruityvice.com/api/fruit/all").body() },
+        getFavoritesApi = { emptyList() },
+        addFavoriteApi = { true },
+        removeFavoriteApi = { true },
     )
 }
 
 private class KtorFruitApi(
-    private val getFruitsFromApi: suspend () -> HttpResponse,
+    private val getFruitsApi: suspend () -> List<FruitSchema>,
+    private val getFavoritesApi: suspend () -> List<Int>,
+    private val addFavoriteApi: suspend (fruitId: Int) -> Boolean,
+    private val removeFavoriteApi: suspend (fruitId: Int) -> Boolean,
 ) : FruitApi {
 
     override suspend fun getFruits(): List<FruitSchema> =
         try {
-            val response = getFruitsFromApi()
-            response.body()
+            getFruitsApi()
         } catch (e: Exception) {
             Log.e("Ktor", e.stackTraceToString())
             emptyList()
+        }
+
+    override suspend fun getFavorites(): List<Int> =
+        try {
+            getFavoritesApi()
+        } catch (e: Exception) {
+            Log.e("Ktor", e.stackTraceToString())
+            emptyList()
+        }
+
+    override suspend fun addFavorite(fruitId: Int): Boolean =
+        try {
+            addFavoriteApi(fruitId)
+        } catch (e: Exception) {
+            Log.e("Ktor", e.stackTraceToString())
+            false
+        }
+
+    override suspend fun removeFavorite(fruitId: Int): Boolean =
+        try {
+            removeFavoriteApi(fruitId)
+        } catch (e: Exception) {
+            Log.e("Ktor", e.stackTraceToString())
+            false
         }
 }
 
@@ -58,6 +113,11 @@ private fun createClient() = HttpClient(CIO) {
             }
         }
         level = LogLevel.ALL
+    }
+    defaultRequest {
+        headers {
+            header("Authorization", "Bearer ${VALID_USERS[0]}")
+        }
     }
     install(ContentNegotiation) {
         json(
