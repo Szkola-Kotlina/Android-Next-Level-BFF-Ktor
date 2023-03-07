fbbbbe75a29391f66c3d711a22c764c9bc2c09d7
AKJAW <akjaworski1@gmail.com>
2023-03-07 05:43:08 +0100

    feat: add initial ktor module

---
+++ ktor/build.gradle.kts
@@ -0,0 +1,35 @@
+val ktor_version: String by project
+val kotlin_version = ext.get("kotlin_version") as String
+val logback_version: String by project
+val exposed_version: String by project
+val h2_version: String by project
+
+plugins {
+    kotlin("jvm")
+    id("io.ktor.plugin") version "2.2.4"
+    id("org.jetbrains.kotlin.plugin.serialization")
+}
+
+group = "com.akjaw"
+version = "0.0.1"
+application {
+    mainClass.set("com.akjaw.ApplicationKt")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-auth-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-resources:$ktor_version")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
+    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
+    implementation("com.h2database:h2:$h2_version")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
---
+++ ktor/src/main/kotlin/com/akjaw/plugins/Security.kt
@@ -0,0 +1,7 @@
+package com.akjaw.plugins
+
+import io.ktor.server.application.*
+
+fun Application.configureSecurity() {
+
+}
